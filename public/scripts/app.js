03e393594c6eb2e24636bbeb44e545b8eba96443
Aliaksei Bychyk <aliaksei_bychyk@epam.com>
2017-09-09 21:53:28 +0300

    exploring JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,11 +3,59 @@
 console.log('App.js is runing!');
 
 var template = React.createElement(
-  'p',
+  'div',
   null,
-  'This is JSX from app.js! And I change it! Does it change!?'
+  React.createElement(
+    'h1',
+    null,
+    'Indecision App'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is some info'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item three'
+    )
+  )
+);
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Aliaksei Bychyk'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: 45'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: Brest, Belarus'
+  )
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 console.log('App.js is runing!')
 
-var template = <p>This is JSX from app.js! And I change it! Does it change!?</p>
+var template = (
+  <div>
+    <h1>Indecision App</h1>
+    <p>This is some info</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+      <li>Item three</li>
+    </ol>
+  </div>
+)
+
+var templateTwo = (
+  <div>
+    <h1>Aliaksei Bychyk</h1>
+    <p>Age: 45</p>
+    <p>Location: Brest, Belarus</p>
+  </div>
+)
 
 var appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(templateTwo, appRoot)
