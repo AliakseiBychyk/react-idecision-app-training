61f1fa8ce9ac10cd9652d3f70af181385648cc15
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-09-11 13:18:58 +0300

    manual data binding

--- src/app.js
+++ src/app.js
@@ -42,26 +42,36 @@ const templateTwo = (
 
 
 let count = 0
+
 const addOne = () => {
-  console.log('addOne')
+  count++
+  renderConterApp()
 }
+
 const minusOne = () => {
-  console.log('minusOne')
+  count--
+  renderConterApp()
 }
+
 const reset = () => {
-  console.log('reset')
+  count = 0
+  renderConterApp()
 }
-
-const templateThree = (
-  <div>
-    <h1>Count: {count}</h1>
-    <button onClick={dddOne}>+1</button>
-    <button onClick={minusOne}>-1</button>
-    <button onClick={reset}>reset</button>
-  </div>
-)
-
 
 const appRoot = document.getElementById('app')
 
-ReactDOM.render(templateThree, appRoot)
+const renderConterApp = () => {
+  const templateThree = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={dddOne}>+1</button>
+      <button onClick={minusOne}>-1</button>
+      <button onClick={reset}>reset</button>
+    </div>
+  )
+
+  ReactDOM.render(templateThree, appRoot)
+}
+
+renderConterApp()
+
