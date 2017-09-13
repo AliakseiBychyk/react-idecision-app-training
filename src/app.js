33154879b6dedccba65fb264ce9d9658bc5963a4
Aliaksei Bychyk <aliaksei_bychyk@epam.com>
2017-09-14 00:14:42 +0300

    creating React component 2

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 class Header extends React.Component {
   render () {
-    return <p>This is from Header</p>
+    return (
+      <div>
+        <h1>Indecision</h1>
+        <h2>Put your life in the hands of a computer</h2>
+      </div>  
+    )
   }
 }
 
@@ -14,10 +19,32 @@ class Actions extends React.Component {
   }
 }
 
+// Options -> Options component here
+// AddOption -> AddOption component here
+
+class Options extends React.Component{
+  render(){
+    return(
+      <div>Options component here</div>
+    )
+  }
+}
+
+class AddOption extends React.Component{
+  render(){
+    return (
+      <div>AddOption compnent here</div>
+    )
+  }
+}
+
+
 const jsx = {
   <div>
     <h1>Title</h1>
     <Header />
+    <Options />
+    <AddOption />
   </div>
 }
 
