b84db94c8d04cc2481163e03ecffa9b7475050be
Aliaksei Bychyk <aliaksei_bychyk@epam.com>
2017-09-17 18:10:27 +0300

    stateless functional component 1

--- src/app.js
+++ src/app.js
@@ -143,4 +143,14 @@ class AddOption extends React.Component {
   }
 }
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
+const User = (props) => {
+  return (
+    <div>
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age}</p>
+    </div>
+  )
+}
+ReactDOM.render(<User name="Andrew" age={26} />, document.getElementById('app'))
+
+// ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
