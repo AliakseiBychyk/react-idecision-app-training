9fb85b6d87f97b6443192a16444daeaae609e5b7
Aliaksei Bychyk <aliaksei_bychyk@epam.com>
2017-09-18 23:21:08 +0300

    source maps with webpack

--- src/app.js
+++ src/app.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom'
 
 import IndecisionApp from './components/IndecisionApp'
 
-
-ReactDOM.render(<IndecisionApp options={['option one', 'option two']}/>, document.getElementById('app'))
+ReactDOM.render(
+  <IndecisionApp options={['option one', 'option two']}/>,
+  document.getElementById('app')
+)
