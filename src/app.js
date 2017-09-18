712efebfae3680265efaa2301ef77d4f97425406
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-09-18 18:07:41 +0300

    importing npm modules

--- src/app.js
+++ src/app.js
@@ -1,22 +1,9 @@
-import './utils.js'
-import subtract, { square, add } from './utils.js'
-import isSenior, { isAdult, canDrink } from './person.js'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import validator from 'validator'
 
-console.log('app.js is running')
-console.log(square(7))
-console.log(add(28, 3485))
-console.log(subtract(100, 85))
+console.log(validator.isEmail('test@gmail.com'))
 
-// person.js
-// named export isAdult(18) - true if adult, otherwise false
-// named export canDrink(18) - true if 21 and over, otherwise false
-// import isAdult and canDrink
-// use both printing result to the console
-
-console.log('isAdult', isAdult(18))
-console.log('canDrink', canDrink(18))
-
-console.log('isSenior', isSenior(18))
-
-// setting the default export and function
-// grab the default and call it 
+// const template = <p>testing 123</p>
+const template = React.createElement('p', {}, 'testing 123')
+ReactDOM.render(template, document.getElementById('app'))
