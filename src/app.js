69f06d4a9995de037c6319d592ed59c9c64d1465
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-09-19 11:08:22 +0300

    passing children to component 1

--- src/app.js
+++ src/app.js
@@ -1,36 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import IndecisionApp from './components/IndecisionApp'
 
-ReactDOM.render(
-  <IndecisionApp options={['option one', 'option two']}/>,
-  document.getElementById('app')
+const Layout = (props) => {
+  return (
+    <div>
+      <p>header</p>
+      {props.content}
+      <p>footer</p>
+    </div>
+  )
+}
+
+const template = (
+  <div>
+    <h1>Page Title</h1>
+    <p>This is my page</p>
+  </div>
 )
 
-class OldSyntax {
-  constructor() {
-    this.name='Mike'
-    this.getGreeting = this.getGreeting.bind(this)
-  }
-
-  getGreeting() {
-    return `Hi. My name is ${this.name}`
-  }
-}
-const oldSyntax = new OldSyntax()
-const oldGetGreeting = oldSyntax.getGreeting
-console.log(oldGetGreeting())  // won't work without binding
-
-// -------------------
-
-class NewSyntax {
-  name = 'Jen'
-
-  getGreeting = () => {
-    return `Hi. My name is ${this.name}`
-  }
-}
-const newSyntax = new NewSyntax()
-const newGetGreeting = newSyntax.getGreeting
-console.log(newGetGreeting())
+ReactDOM.render(
+  <Layout content={template}/>,
+  document.getElementById('app')
+)
