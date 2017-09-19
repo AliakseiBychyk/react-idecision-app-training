40ba5a65511502a1b240f1891311daf905bf99b1
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-09-19 14:15:34 +0300

    setting up webpack with scss

--- src/components/Options.js
+++ src/components/Options.js
@@ -5,8 +5,7 @@ const Options = (props) => (
   <div>
     <button onClick={props.handleDeleteOptions} >Remove All</button>
     {props.options.length === 0 && <p>Please add an option to get started!</p>}
-    {
-      props.options.map(option =>(
+    {props.options.map(option =>(
         <Option 
           key={option} 
           text={option}
